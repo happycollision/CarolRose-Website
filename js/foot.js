419f46ae54d029e72511aec04f19a32d4dbf2cbb
Don Denton <don@happycollision.com>
2014-12-11 18:18:11 -0600

    Adds respondr.

--- js/foot.js
+++ js/foot.js
@@ -2,3 +2,10 @@
 ---
 
 {% include_relative _js/main-nav.js %}
+{% include bower_components/respondr/dist/jquery.respondr.min.js %}
+
+$.respondr({
+  apiKey: 'c2dfff46955d313d86bcd1a051f57af7'
+})
+
+$('.page-content').respondr();
