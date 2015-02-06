8ff2351a9420f7b65b63465d54bb32fc5143b4c0
Don Denton <don@happycollision.com>
2015-02-05 18:21:02 -0600

    Moves respondr() call to default.html
    Allows for specialized calls of that script for each page.

--- js/foot.js
+++ js/foot.js
@@ -5,15 +5,15 @@
 {% include bower_components/respondr/dist/jquery.respondr.min.js %}
 {% include bower_components/fitvids/jquery.fitvids.js %}
 
+$.respondr({
+  apiKey: 'c2dfff46955d313d86bcd1a051f57af7',
+  usePicturefill: true
+});
+
+
 $(document).ready(function(){
   
-  $.respondr({
-    apiKey: 'c2dfff46955d313d86bcd1a051f57af7',
-    usePicturefill: true
-  });
-
   $('.page-content').respondr();  
-
 
   $('.page-content').fitVids();
 
